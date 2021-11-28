9e3e9b5b34f426518552eb6d3d17985b5e696e5e
duongvituan <duongvituan99@gmail.com>
2021-11-28 15:09:06 +0700

    Create Demo Transform Fx

---
+++ godot/demo/list_demo/transfrom/demo.gd
@@ -0,0 +1,15 @@
+extends DemoScreen
+
+
+func get_name() -> String:
+	return "Transform"
+
+
+func _ready():
+	$Char2/EzTransfromGold.is_active = true
+	
+	$Char3/EzTransformMetal.is_active = true
+	
+	$Char4/EzTransformGhost.is_active = true
+	
+	$Char5/EzTransfromFreeze.is_active = true
